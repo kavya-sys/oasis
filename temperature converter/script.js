8a1ca22d92fdac94d7439fb2f663fbb314d41637
kavya-sys <140872033+kavya-sys@users.noreply.github.com>
2023-09-06 01:34:57 +0530

    Add files via upload

---
+++ temperature converter/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const celsiusInput = document.getElementById('celsius');
+    const fahrenheitInput = document.getElementById('fahrenheit');
+    const convertBtn = document.getElementById('convertBtn');
+
+    convertBtn.addEventListener('click', () => {
+        const celsiusValue = parseFloat(celsiusInput.value);
+        const fahrenheitValue = parseFloat(fahrenheitInput.value);
+
+        if (!isNaN(celsiusValue)) {
+            const convertedFahrenheit = (celsiusValue * 9/5) + 32;
+            fahrenheitInput.value = convertedFahrenheit.toFixed(2);
+        }
+
+        if (!isNaN(fahrenheitValue)) {
+            const convertedCelsius = (fahrenheitValue - 32) * 5/9;
+            celsiusInput.value = convertedCelsius.toFixed(2);
+        }
+    });
+});
